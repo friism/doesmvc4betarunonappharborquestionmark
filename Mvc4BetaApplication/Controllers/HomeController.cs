6bfe3f5880e6ccc5da8561cebadaa0b642e8c089
Michael Friis <friism@gmail.com>
2012-02-16 15:53:32 -0800

    initial commit

---
+++ Mvc4BetaApplication/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace Mvc4BetaApplication.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Version = Assembly.GetAssembly(typeof(Controller)).GetName().Version.ToString();
+			return View();
+		}
+	}
+}
---
+++ Mvc4BetaApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc4BetaApplication.MvcApplication" Language="C#" %>
